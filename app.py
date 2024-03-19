ec28f9603e294fd2d12bea9322d33e1099bc12a1
insolor <2442833+insolor@users.noreply.github.com>
2024-03-19 20:59:28 +0300

    Added language selector

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 from package_build.metadata import get_hook_metadata, get_dict_metadata
-
+from package_build.models import DictInfoEntry
 
 hook_metadata = get_hook_metadata()
 
-st.write(hook_metadata)
-
 dict_metadata = get_dict_metadata()
 
-st.write(dict_metadata)
+selected_dict_entry: DictInfoEntry = st.selectbox(label="Choose language", options=dict_metadata)
