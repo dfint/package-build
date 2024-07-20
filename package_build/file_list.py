a1141dd4146e30511ba79c193342e4c53004176c
insolor <2442833+insolor@users.noreply.github.com>
2024-07-20 18:37:33 +0300

    Exit if no files available

--- package_build/file_list.py
+++ package_build/file_list.py
@@ -11,6 +11,7 @@ def show_file_list(root_dir: Path) -> None:
 
     if not list(root_dir.glob("*.zip")):
         st.write("No package files available.")
+        return
 
     column1, column2, column3 = st.columns([3, 2, 1], vertical_alignment="center")
     column1.write("Package name")
