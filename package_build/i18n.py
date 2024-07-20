a7c02a8783b1facfff44d6fd6f1513218b73f885
insolor <2442833+insolor@users.noreply.github.com>
2024-07-20 22:26:22 +0300

    Get preferred languages from browser

--- package_build/i18n.py
+++ package_build/i18n.py
@@ -1,13 +1,28 @@
 import gettext
+import time
+from functools import cache
 from pathlib import Path
+
+from streamlit_javascript import st_javascript
+
+
+@cache
+def get_preferred_languages() -> list[str]:
+    while True:
+        languages = st_javascript("navigator.languages")
+        if languages:
+            return languages
+
+        time.sleep(0.1)
+
 
 locale_dir = Path(__file__).parent / "locale"
 
-locale = "ru"
+languages = get_preferred_languages()
 lang = gettext.translation(
     "messages",
     localedir=str(locale_dir),
-    languages=[locale],
+    languages=languages,
     fallback=True,
 )
 
