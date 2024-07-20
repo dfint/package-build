82388d375e84f3b67e5c812ee65e57b3a7165aed
insolor <2442833+insolor@users.noreply.github.com>
2024-07-20 22:35:04 +0300

    Fix errors and warnings when getting preferred languages

--- package_build/i18n.py
+++ package_build/i18n.py
@@ -1,19 +1,19 @@
 import gettext
 import time
-from functools import cache
 from pathlib import Path
 
+import streamlit as st
 from streamlit_javascript import st_javascript
 
+with st.empty():
+    @st.cache_resource
+    def get_preferred_languages() -> list[str]:
+        while True:
+            languages = st_javascript("navigator.languages")
+            if languages:
+                return languages
 
-@cache
-def get_preferred_languages() -> list[str]:
-    while True:
-        languages = st_javascript("navigator.languages")
-        if languages:
-            return languages
-
-        time.sleep(0.1)
+            time.sleep(0.1)
 
 
 locale_dir = Path(__file__).parent / "locale"
