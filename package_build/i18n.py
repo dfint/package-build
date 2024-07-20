ff86d27cd4fcff0a6e3380a24b84e90d703022df
insolor <2442833+insolor@users.noreply.github.com>
2024-07-20 22:52:27 +0300

    Trying to fix

--- package_build/i18n.py
+++ package_build/i18n.py
@@ -2,23 +2,22 @@ import gettext
 import time
 from pathlib import Path
 
-import streamlit as st
 from streamlit_javascript import st_javascript
 
-with st.empty():
-    @st.cache_resource
-    def get_preferred_languages() -> list[str]:
-        while True:
-            languages = st_javascript("navigator.languages")
-            if languages:
-                return languages
 
-            time.sleep(0.1)
+def get_preferred_languages() -> list[str]:
+    while True:
+        languages = st_javascript("navigator.languages")
+        if languages:
+            return languages
+
+        time.sleep(0.1)
 
 
 locale_dir = Path(__file__).parent / "locale"
 
 languages = get_preferred_languages()
+
 lang = gettext.translation(
     "messages",
     localedir=str(locale_dir),
