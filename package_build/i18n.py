1a704189fdd4acd2631b17f2cc187b456b2e12cb
insolor <2442833+insolor@users.noreply.github.com>
2024-07-21 11:56:12 +0300

    Process cases when headers are empty or Accept-Language is not set

--- package_build/i18n.py
+++ package_build/i18n.py
@@ -6,8 +6,9 @@ from streamlit.web.server.websocket_headers import _get_websocket_headers
 
 
 def get_preferred_languages() -> list[str]:
-    headers = _get_websocket_headers()
-    return re.findall(r"([a-zA-Z-]{2,})", headers["Accept-Language"]) or []
+    headers = _get_websocket_headers() or {}
+    accept_language = headers.get("Accept-Language") or ""
+    return re.findall(r"([a-zA-Z-]{2,})", accept_language) or []
 
 
 locale_dir = Path(__file__).parent / "locale"
