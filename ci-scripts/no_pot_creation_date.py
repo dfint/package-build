66f2597dc6a0d8ab83f65e618d41ef0614aea31c
insolor <2442833+insolor@users.noreply.github.com>
2024-07-23 23:25:19 +0300

    Specify encoding explicitly

--- ci-scripts/no_pot_creation_date.py
+++ ci-scripts/no_pot_creation_date.py
@@ -4,7 +4,7 @@ pot_path = Path(__file__).parent.parent / "package_build" / "locale" / "messages
 
 data = pot_path.read_text("utf-8").splitlines(keepends=True)
 
-with pot_path.open("w") as pot_file:
+with pot_path.open("w", encoding="utf-8") as pot_file:
     for line in data:
         if line.startswith('"POT-Creation-Date:'):
             continue
