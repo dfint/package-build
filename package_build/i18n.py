a7ee3943b259ddda8eb63c95fb0849730336843f
insolor <2442833+insolor@users.noreply.github.com>
2025-01-15 14:37:14 +0300

    Use streamlit-gettext package

--- package_build/i18n.py
+++ package_build/i18n.py
@@ -1,45 +1,9 @@
-import gettext as gettext_module
-import re
-from functools import lru_cache
 from pathlib import Path
 
-import streamlit as st
-from loguru import logger
+from streamlit_gettext import GettextWrapper
 
+current_path = Path(__file__).parent
+_gettext_wrapper = GettextWrapper(locale_path=current_path / "locale")
 
-def get_preferred_languages() -> list[str]:
-    accept_language = st.context.headers.get("Accept-Language") or ""
-    return re.findall(r"([a-zA-Z-]{2,})", accept_language) or []
-
-
-@lru_cache(maxsize=128, typed=False)
-def _get_lang(languages: tuple[str]) -> gettext_module.NullTranslations:
-    logger.info(f"Languages: {languages}")
-    locale_dir = Path(__file__).parent / "locale"
-
-    return gettext_module.translation(
-        "messages",
-        localedir=locale_dir,
-        languages=languages,
-        fallback=True,
-    )
-
-
-
-class LanguageWrapper:
-    @staticmethod
-    def _get_lang() -> gettext_module.NullTranslations:
-        return _get_lang(tuple(get_preferred_languages()))
-
-    def gettext(self, message: str) -> str:
-        lang = self._get_lang()
-        return lang.gettext(message)
-
-    def ngettext(self, singular: str, plural: str, n: int) -> str:
-        lang = self._get_lang()
-        return lang.ngettext(singular, plural, n)
-
-
-lang = LanguageWrapper()
-gettext = lang.gettext
-ngettext = lang.ngettext
+gettext = _gettext_wrapper.gettext
+ngettext = _gettext_wrapper.ngettext
